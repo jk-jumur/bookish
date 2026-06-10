0fd49a8a05b71ec1097f9ab4b4e2d7276d315a04
jk-jumur <developer.jumur@gmail.com>
2026-06-10 12:36:27 +0600

    bookish project setup

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+const router = createBrowserRouter([
+  
+  {
+     path: "/",
+     element: "HomePage"
+
+},
+
+{
+   path:"books",
+   element: "books"
+}
+
+  
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
